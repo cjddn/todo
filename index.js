8eea4659dcbb524e9e815e5174e1d891a22307d4
김청우 <rhakckato12@naver.com>
2023-07-13 13:47:27 +0900

    todo insert

--- index.js
+++ index.js
@@ -2,36 +2,55 @@ const express = require('express')
 const app = express()
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 const uri = process.env.MONGODB_URI
 const { UUID } = require('bson');
 const client = new MongoClient(uri, {
-  pkFactory: { createPk: () =>  new UUID().toBinary() }
+    pkFactory: { createPk: () => new UUID().toBinary() }
 });
 async function main() {
-  try {
-    await client.connect()
-    app.listen(8080, function() {
-        console.log('listening on 8080')
-    })
-  } finally {
-    await client.close();
-  }
+    try {
+        await client.connect()
+        app.listen(8080, function () {
+            console.log('listening on 8080')
+        })
+    } finally {
+        await client.close();
+    }
 }
 main().catch(console.dir);
 
-async function insertData(){
-  const database = client.db('todo');
-  const collection = database.collection('post');
-  let autoIncrement = await collection.countDocuments({}, { hint: "_id_" }) + 1;
-  const insertResult = await collection.insertOne({id: autoIncrement, a: 1});
-  console.log('저장 결과 =>', insertResult);
+async function insertData(datas) {
+    try {
+        await client.connect()
+        const database = client.db('todo');
+        const collection = database.collection('post');
+        let autoIncrement = await collection.countDocuments({}, { hint: "_id_" }) + 1;
+        const currentDate = new Date().toISOString().substring(0, 10);
+        datas.id = autoIncrement;
+        datas.createdAt = currentDate;
+        const insertResult = await collection.insertOne(datas);
+        return insertResult;
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await client.close();
+    }
+
 }
-// const findData = await movies.findOne({id: 10});
-// console.log(findData._id.toString());
-app.get('/', function(req, res) { 
-  res.render('index.ejs')
+
+app.get('/', function (req, res) {
+    res.render('index.ejs')
 })
 
-app.get('/list', function(req, res) { 
-  res.send('리스트 페이지')
-})
+app.post('/post', async function (req, res) {
+    try {
+        const result = await insertData(req.body);
+        console.log(result);
+        res.status(200).json({ message: '저장 완료' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error });
+    }
+});
