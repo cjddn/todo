0390567b170c1e2e9d75b37453ba7274cd77b707
김청우 <rhakckato12@naver.com>
2023-07-25 17:15:16 +0900

    폴더별 파일 관리

--- index.js
+++ index.js
@@ -1,133 +1,25 @@
 const express = require('express')
 const app = express()
-const { MongoClient } = require("mongodb");
-require("dotenv").config();
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-const uri = process.env.MONGODB_URI
-const { UUID } = require('bson');
-const client = new MongoClient(uri, {
-    pkFactory: { createPk: () => new UUID().toBinary() }
-});
+app.use('/public', express.static('public'));
+const postRouter = require('./routes/post.js');
+app.use('/post', postRouter);
+
+
 async function main() {
     try {
-        await client.connect()
+        await postRouter.client.connect()
         app.listen(8080, function () {
             console.log('listening on port 8080')
         })
     } finally {
-        await client.close();
+        await postRouter.client.close();
     }
 }
 main().catch(console.dir);
 
-async function insertData(datas) {
-    try {
-        await client.connect()
-        const database = client.db('todo');
-        const collection = database.collection('post');
-        let autoIncrement = await collection.countDocuments({}, { hint: "_id_" }) + 1;
-        const currentDate = new Date().toISOString().substring(0, 10);
-        datas.id = autoIncrement;
-        datas.createdAt = currentDate;
-        datas.isCompleted = 'n';
-        const insertResult = await collection.insertOne(datas);
-        return insertResult;
-    } catch (error) {
-        console.log(error);
-    } finally {
-        await client.close();
-    }
 
-}
-async function findData(where = {}) {
-    try {
-        await client.connect()
-        const database = client.db('todo');
-        const collection = database.collection('post');
-        const findResult = await collection.find(where).toArray();
-        return findResult;
-    } catch (error) {
-        console.log(error);
-    } finally {
-        await client.close();
-    }
-}
-async function updateData(id, updateData) {
-    try {
-        var findResult = await findData({id:parseInt(id)});
-        const _id = findResult[0]._id;
-        await client.connect()
-        const database = client.db('todo');
-        const collection = database.collection('post'); 
-        const updateResult = await collection.updateOne( {_id : _id}, {$set : updateData})
-        return updateResult;
-    } catch (error) {
-        console.log(error);
-    } finally {
-        await client.close();
-    }
-}
-async function deleteData(id) {
-    try {
-        var findResult = await findData({id:parseInt(id)});
-        const _id = findResult[0]._id;
-        await client.connect()
-        const database = client.db('todo');
-        const collection = database.collection('post'); 
-        const deleteResult = await collection.deleteOne( {_id : _id});
-        return deleteResult;
-    } catch (error) {
-        console.log(error);
-    } finally {
-        await client.close();
-    }
-}
 app.get('/', async function (req, res) {
-    const todoList = await findData();
+    const todoList = await postRouter.findData();
     res.render('index.ejs',{list:todoList});
 })
 
-app.get('/post', async function (req, res) {
-    try {
-        const where = req.query.isCompleted ? req.query : {}
-        const todoList = await findData(where);
-        console.log(todoList);
-        res.status(200).json({ list: todoList });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error });
-    }
-})
-app.post('/post', async function (req, res) {
-    try {
-        const result = await insertData(req.body);
-        console.log(result);
-        res.status(200).json({ message: '저장 완료' });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error });
-    }
-});
-
-app.put('/post', async function (req, res) {
-    try {
-        const result = await updateData(req.body.id, req.body.updateData);
-        console.log(result);
-        res.status(200).json({ message: '수정 완료' });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error });
-    }
-});
-
-app.delete('/post', async function (req, res) {
-    try {
-        const result = await deleteData(req.body.id);
-        console.log(result);
-        res.status(200).json({ message: '삭제 완료' });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error });
-    }
-});
---
+++ routes/post.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const router = express.Router();
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+router.use(express.json())
+router.use(express.urlencoded({ extended: true }))
+const uri = process.env.MONGODB_URI
+const { UUID } = require('bson');
+const client = new MongoClient(uri, {
+    pkFactory: { createPk: () => new UUID().toBinary() }
+});
+async function insertData(datas) {
+    try {
+        await client.connect()
+        const database = client.db('todo');
+        const collection = database.collection('post');
+        let autoIncrement = await collection.countDocuments({}, { hint: "_id_" }) + 1;
+        const currentDate = new Date().toISOString().substring(0, 10);
+        datas.id = autoIncrement;
+        datas.createdAt = currentDate;
+        datas.isCompleted = 'n';
+        const insertResult = await collection.insertOne(datas);
+        return insertResult;
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await client.close();
+    }
+
+}
+async function findData(where = {}) {
+    try {
+        await client.connect()
+        const database = client.db('todo');
+        const collection = database.collection('post');
+        const findResult = await collection.find(where).toArray();
+        return findResult;
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await client.close();
+    }
+}
+async function updateData(id, updateData) {
+    try {
+        const findResult = await findData({id:parseInt(id)});
+        const _id = findResult[0]._id;
+        await client.connect();
+        const database = client.db('todo');
+        const collection = database.collection('post'); 
+        const updateResult = await collection.updateOne( {_id : _id}, {$set : updateData});
+        return updateResult;
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await client.close();
+    }
+}
+async function deleteData(id) {
+    try {
+        const findResult = await findData({id:parseInt(id)});
+        const _id = findResult[0]._id;
+        await client.connect();
+        const database = client.db('todo');
+        const collection = database.collection('post'); 
+        const deleteResult = await collection.deleteOne( {_id : _id});
+        return deleteResult;
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await client.close();
+    }
+}
+router.get('/', async function (req, res) {
+    try {
+        const where = req.query.isCompleted ? req.query : {}
+        const todoList = await findData(where);
+        console.log(todoList);
+        res.status(200).json({ list: todoList });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error });
+    }
+})
+router.post('/', async function (req, res) {
+    try {
+        const result = await insertData(req.body);
+        console.log(result);
+        res.status(200).json({ message: '저장 완료' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error });
+    }
+});
+
+router.put('/', async function (req, res) {
+    try {
+        const result = await updateData(req.body.id, req.body.updateData);
+        console.log(result);
+        res.status(200).json({ message: '수정 완료' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error });
+    }
+});
+
+router.delete('/', async function (req, res) {
+    try {
+        const result = await deleteData(req.body.id);
+        console.log(result);
+        res.status(200).json({ message: '삭제 완료' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error });
+    }
+});
+
+module.exports = router;
+module.exports.findData = findData;
+module.exports.client = client;
